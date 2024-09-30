68e0e5f397a1edd5e9aac765c4e55012c03a7ef4
Matheus Caminhotto <matheus.caminhotto@rtm.net.br>
2024-09-30 20:48:50 -0300

    add RDS tf

---
+++ RDS.tf
@@ -0,0 +1,84 @@
+
+# Configure the VPC
+resource "aws_vpc" "mainRDS" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "rds-vpc"
+  }
+}
+
+resource "aws_subnet" "publicRDS" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "rds-public-subnet"
+  }
+}
+
+# Create the security group for the RDS instance
+resource "aws_security_group" "rds_sg" {
+  name   = "rds-sqlserver-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 1433
+    to_port     = 1433
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_subnet_group" "main" {
+  name       = "rds-sqlserver-subnet-group"
+  subnet_ids = ["vpc-pvdzosqeerps"]
+
+  tags = {
+    Name = "rds-sqlserver-subnet-group"
+  }
+}
+
+
+
+# Create the RDS instance
+resource "aws_db_instance" "main" {
+  allocated_storage = 10
+  engine             = "sqlserver-ee"
+  engine_version     = "SQL Server 2019 Developer"
+  instance_class     = "db.t3.micro"
+  identifier         = "rds-sqlserver"
+  db_name            = "fiap-sa"
+  password           = var.rds_password
+  port               = 1433
+  skip_final_snapshot = true
+  username           = "admin"
+
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.main.name
+
+  tags = {
+    Name = "rds-sqlserver"
+  }
+}
+
+# Definir a variável para a senha do RDS
+variable "rds_password" {
+  type = string
+}
+
+# Definir as saídas
+output "rds_endpoint" {
+  value = aws_db_instance.main.address
+}
+
+output "rds_password" {
+  value = var.rds_password
+}
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# fiap-lanchonete-terraform-rds
-Infraestrutura do banco de dados na aws
+# Terraform AWS RDS Module
+
+Este repositório contém código Terraform para provisionar e configurar um banco de dados Amazon RDS na AWS.
+
+## Índice
+
+- [Pré-requisitos](#pré-requisitos)
+- [Recursos Provisionados](#recursos-provisionados)
+
+## Pré-requisitos
+
+Antes de usar este repositório, você precisa ter os seguintes requisitos atendidos:
+
+- [Terraform](https://www.terraform.io/downloads.html) versão >= 1.0.0
+- Uma conta na [AWS](https://aws.amazon.com/)
+- Configuração do [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-quickstart.html)
+- Credenciais válidas no `~/.aws/credentials`
+
+## Recursos Provisionados
+
+Este módulo do Terraform cria e configura os seguintes recursos na AWS:
+
+- Amazon RDS Instance (SQL Server)
+- Subnet Group para o RDS
+- Security Group para o acesso ao RDS

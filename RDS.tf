a31be99b7c085763d78158d8689bb8d30911727a
Guilherme Felipe Martins <82408655+guilherme-martins92@users.noreply.github.com>
2024-10-01 21:13:16 -0300

    Update RDS.tf

--- RDS.tf
+++ RDS.tf
@@ -56,7 +56,7 @@ resource "aws_db_instance" "main" {
   instance_class     = "db.t3.micro"
   identifier         = "rds-sqlserver"
   db_name            = "fiap-sa"
-  password           = @Fiap123
+  password           = "@Fiap123"
   port               = 1433
   skip_final_snapshot = true
   username           = "admin"
